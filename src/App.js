015c6c69a8ad0e1a767e801668c11b9683b0ff83
Your Name <leobarrientos02@gmail.com>
2021-12-26 22:22:27 -0500

    React Redux Practice
    Practicing using redux in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const signedIn = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch({type: "INCREMENT"}) }>Increment</button>
+      <Nav />
+      {signedIn &&(
+        <h1>MOVIE LIST</h1>
+      )}
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "./Search";
+
+const Nav = () =>{
+    return(
+        <nav>
+            <h3>Nav</h3>
+            <Search />
+        </nav>
+    );
+}
+export default Nav;
